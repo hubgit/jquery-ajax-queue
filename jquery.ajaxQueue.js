6f53b342f54bad38ce49c00610b6ddc6e2000fae
Alf Eaton <alf@hubmed.org>
2014-02-20 21:18:15 +0000

    named queues

--- jquery.ajaxQueue.js
+++ jquery.ajaxQueue.js
@@ -12,19 +12,26 @@
     $.ajaxQueue = function(params, options) {
         var item = $.extend(new QueueItem(params), options);
 
-        if (item.priority) {
-            queue.items.unshift(item);
-        } else {
-            queue.items.push(item);
+        if (!queues[item.queue]) {
+            queues[item.queue] = new Queue;
         }
 
-        queue.next();
+        item.queue = queues[item.queue];
+
+        if (item.priority) {
+            item.queue.items.unshift(item);
+        } else {
+            item.queue.items.push(item);
+        }
+
+        item.queue.next();
 
         return item.deferred.promise();
     };
 
-    $.ajaxQueue.concurrent = function(concurrent) {
-        queue.concurrent = concurrent;
+    $.ajaxQueue.concurrent = function(concurrent, queue) {
+        queue = queue || 'default';
+        queues[queue].concurrent = concurrent;
     };
 
     var QueueItem = function(params) {
@@ -34,40 +41,41 @@
         this.limit = 0;
         this.delay = { rate: 10000, server: 5000 };
         this.deferred = $.Deferred();
+        this.queue = 'default';
     };
 
     QueueItem.prototype.run = function() {
         var item = this;
-        
+
         var request = $.ajax(item.params);
 
         item.deferred.notify(request, 'start', item);
 
         request.done(function(data, textStatus, jqXHR) {
-            queue.currentCount--;
+            item.queue.currentCount--;
             item.deferred.resolve(data, textStatus, jqXHR);
-            
+
             window.setTimeout(function() {
-                queue.next();
+                item.queue.next();
             }, item.limit);
         });
 
         request.fail(function(jqXHR, textStatus, errorThrown) {
-            queue.currentCount--;
+            item.queue.currentCount--;
 
             switch (jqXHR.status) {
                 case 403: // rate-limited
-                    queue.stop(item.delay.rate);
-                    queue.items.unshift(item); // add this item back to the queue
+                    item.queue.stop(item.delay.rate);
+                    item.queue.items.unshift(item); // add this item back to the queue
                     item.deferred.notify(jqXHR, 'rate-limit', item);
                     break;
 
                 case 500: // server error
                 case 503: // unknown error
-                    queue.stop(item.delay.server);
+                    item.queue.stop(item.delay.server);
 
                     if (--item.tries) {
-                        queue.items.unshift(item); // add this item back to the queue
+                        item.queue.items.unshift(item); // add this item back to the queue
                         item.deferred.notify(jqXHR, 'retry', item);
                     } else {
                         item.deferred.reject(jqXHR, textStatus, errorThrown);
@@ -76,47 +84,51 @@
 
                 default:
                     item.deferred.reject(jqXHR, textStatus, errorThrown);
-                    queue.next();
+                    item.queue.next();
                     break;
             }
         });
     };
 
-    var queue = {
-        items: [],
-        concurrent: 1,
-        currentCount: 0,
-        stopped: false,
+    var Queue = function() {
+        this.items = [];
+        this.concurrent = 1;
+        this.currentCount = 0;
+        this.stopped = false;
+    };
 
-        stop: function(delay) {
-            this.stopped = true;
+    Queue.prototype.stop = function(delay) {
+        this.stopped = true;
 
-            if (delay) {
-                window.setTimeout(function() {
-                    queue.start();
-                }, delay);
-            }
-        },
+        if (delay) {
+            var queue = this;
 
-        start: function()  {
-            this.stopped = false;
-            this.next();
-        },
-
-        clear: function(){
-            this.items = [];
-            this.currentCount = 0;
-        },
-
-        next: function() {
-            if (this.stopped) {
-                return;
-            }
-
-            while (this.items.length && this.currentCount < this.concurrent) {
-                this.currentCount++;
-                this.items.shift().run();
-            }
+            window.setTimeout(function() {
+                queue.start();
+            }, delay);
         }
     };
+
+    Queue.prototype.start = function()  {
+        this.stopped = false;
+        this.next();
+    };
+
+    Queue.prototype.clear = function(){
+        this.items = [];
+        this.currentCount = 0;
+    };
+
+    Queue.prototype.next = function() {
+        if (this.stopped) {
+            return;
+        }
+
+        while (this.items.length && this.currentCount < this.concurrent) {
+            this.currentCount++;
+            this.items.shift().run();
+        }
+    };
+
+    var queues = { default: new Queue };
 })(jQuery);
